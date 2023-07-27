2dfcc8eac41b1c62fc6fb137a291604b3162c20a
Adro Morelos <me@adro.rocks>
2023-07-26 22:16:59 -0500

    chore(clean) - Remove unsued code.

--- tests/Unit/ValidatorTest.php
+++ tests/Unit/ValidatorTest.php
@@ -28,25 +28,3 @@ test('validate domain', function () {
     $this->assertSame('com.mx', $array['tld']);
     $this->assertSame('www.adro.com.mx', $array['host']);
 });
-
-// public function testValidate(): void
-// {
-//     $mock = new MockHandler ([
-//         new Response(
-//             status: 200,
-//             headers: ['content-type' => 'application/json'],
-//             body: '{"valid":true,"original":"https:\/\/api.domainvalidity.dev","host":"api.domainvalidity.dev","domain":"domainvalidity.dev","tld":"dev"}'
-//         ),
-//     ]);
-//     $handlerStack = HandlerStack::create($mock);
-//     $client = new Client([
-//         'handler' => $handlerStack,
-//         'base_uri' => Factory::PRODUCTION_HOST,
-//     ]);
-//     $validator = new Validator($client);
-//     $host = $validator->validate(Factory::PRODUCTION_HOST);
-//     $this->assertInstanceOf(Host::class, $host);
-//     $this->assertSame(Factory::PRODUCTION_HOST, $host->original());
-//     $this->assertSame('dev', $host->tld());
-//     $this->assertSame('domainvalidity.dev', $host->domain());
-// }
