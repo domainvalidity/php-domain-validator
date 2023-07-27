ae4f2f7f8c4838f93ca0b5b4a08b1d4bc19c45dc
Adro Morelos <me@adro.rocks>
2023-07-26 23:12:00 -0500

    chore(tests) - update pest test.

--- tests/Unit/ValidatorTest.php
+++ tests/Unit/ValidatorTest.php
@@ -1,30 +1,15 @@
 <?php
 
-use DomainValidity\Factory;
-use DomainValidity\Validator;
+use DomainValidity\Host\Host;
 
-test('instance', function () {
-    $validator = Factory::make(
-        getPublicSuffixListPath()
-    );
+describe('Validator', function () {
+    $validator = getInstance();
+    $url = 'https://www.adro.com.mx/';
+    $host = $validator->validate($url);
 
-    expect($validator)->toBeInstanceOf(Validator::class);
-});
-
-/** @var \PHPUnit\Framework\TestCase $this */
-test('validate domain', function () {
-    $validator = Factory::make(
-        getPublicSuffixListPath()
-    );
-
-    $domainName = 'https://www.adro.com.mx/';
-
-    $array = $validator->validate($domainName);
-
-    expect($array)->toBeArray();
-
-
-    $this->assertSame($domainName, $array['original']);
-    $this->assertSame('com.mx', $array['tld']);
-    $this->assertSame('www.adro.com.mx', $array['host']);
+    it('is instance of host', fn () =>  expect($host)->toBeInstanceOf(Host::class));
+    it("is same as $url", fn () =>  expect($host->original())->toBe($url));
+    it('com.mx is the TLD', fn () =>  expect($host->tld())->toBe('com.mx'));
+    it('adro.com.mx is the domain', fn () =>  expect($host->domain())->toBe('adro.com.mx'));
+    it('www.adro.com.mx is the host', fn () =>  expect($host->toString())->toBe('www.adro.com.mx'));
 });
